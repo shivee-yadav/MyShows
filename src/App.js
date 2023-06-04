56fa5a6d7d4f3bb062e843cdafc38ce0f784e744
shivee-yadav <shiveeyadav089@gmail.com>
2023-06-04 21:04:46 +0530

    Colors

--- src/App.js
+++ src/App.js
@@ -3,21 +3,9 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App p-10">
+      <h1 className='text-6xl text-purpy-500'>Hello</h1>
+      <button className='border-2 px-2 py-1 border-blue-500 md:border-red-500 lg:border-green-500 rounded bg-blue-500 md:bg-red-500  lg:bg-green-500 text-white hover:bg-transparent hover:text-blue-500 md:hover:text-red-500 lg:hover:text-green-500 hover:border-2'>Taylor Swift!</button>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        purpy:
+
+        {
+          50: '#f9e4ff',
+          100: '#e8b3ff',
+          200: '#d881fd',
+          300: '#c84ffc',
+          400: '#b921fc',
+          500: '#a00ee3',
+          600: '#7c07b1',
+          700: '#59037e',
+          800: '#36004d',
+          900: '#14001d',
+        }
+
+      }
+    },
   },
   plugins: [],
 }
