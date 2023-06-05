71ee10fc30ed47d0bc6d298e5cfa19115545acf7
shivee-yadav <shiveeyadav089@gmail.com>
2023-06-05 15:37:04 +0530

    New pages added

---
+++ planning.js
@@ -0,0 +1,21 @@
+//Book my Show 
+
+// we need a reusable caraoussel
+//HERO caraousel
+//common caraousel
+//cast caraousel
+
+//Pages
+
+//home page
+//Movie page
+//categories page
+//Auth page
+//RazorPay payment gateway
+
+//Plan how we are building it?
+
+//layouts
+//routes
+//pages
+//components
---
+++ src/layouts/Default.layout.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const DefaultLayout = () => {
+    return (
+        <h1 className="text-xl">Default Layout Page</h1>
+    );
+};
+
+export default DefaultLayout;
