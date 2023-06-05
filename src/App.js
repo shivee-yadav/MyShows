27edd3a21da048727ccd92f06ad6dca8b378b752
shivee-yadav <shiveeyadav089@gmail.com>
2023-06-05 16:13:14 +0530

    Added route

--- src/App.js
+++ src/App.js
@@ -1,13 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes } from "react-router-dom";
+
+function Name() {
+  return <h1>Welcom to Home Page!!!</h1>
+}
+
+function Name2() {
+  return <h1>Welcom to Movie Page!!!</h1>
+}
 
 function App() {
   return (
-    <div className="App p-10">
-      <h1 className='text-6xl text-purpy-500'>Hello</h1>
-      <button className='border-2 px-2 py-1 border-blue-500 md:border-red-500 lg:border-green-500 rounded bg-blue-500 md:bg-red-500  lg:bg-green-500 text-white hover:bg-transparent hover:text-blue-500 md:hover:text-red-500 lg:hover:text-green-500 hover:border-2'>Taylor Swift!</button>
-    </div>
-  );
-}
+    <Routes>
+      <Route path="/" exact Component={Name} />
+      <Route path="/movie" exact Component={Name2}/>
+    </Routes>//fragments;to return;empty tags
+    );
+  }
+  
+  export default App;
 
-export default App;
+//exact -> to show the exact page where we want to render
+//SPREAD operator:for adding new parameters without creating another object
+
+
+
+
+
+
+  // <div className="App p-10">
+  //   <h1 className='text-6xl text-purpy-500'>Hello</h1>
+  //   <button className='border-2 px-2 py-1 border-blue-500 md:border-red-500 lg:border-green-500 rounded bg-blue-500 md:bg-red-500  lg:bg-green-500 text-white hover:bg-transparent hover:text-blue-500 md:hover:text-red-500 lg:hover:text-green-500 hover:border-2'>Taylor Swift!</button>
+  // </div>
---
+++ src/HOC/Default.HOC.js
@@ -0,0 +1,10 @@
+//transforms a component into another component
+//adding aditional functionalitites to the existing component
+
+import React from "react";
+
+const DefaultHOC = () => {
+    return <div></div>
+};
+
+export default DefaultHOC;
