054b1d70adf24484b5481a9ee314093309587657
shivee-yadav <shiveeyadav089@gmail.com>
2023-06-11 22:05:48 +0530

    new files added

---
+++ src/Pages/Movie.page.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Movie = () => {
+    return (
+        <>
+        <div>
+            <img src="https://assets-in.bmscdn.com/iedb/movies/images/mobile/listing/medium/transformers-rise-of-the-beasts-et00346319-1682680068.jpg"
+            alt="Transformers: The rise of the beasts"
+            />
+        </div>
+        </>
+    )
+};
+
+export default Movie;
