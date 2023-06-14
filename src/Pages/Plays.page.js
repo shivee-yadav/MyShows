decb5aabcddd29bd7f7517d31118a449dcd77a22
shivee-yadav <shiveeyadav089@gmail.com>
2023-06-14 19:26:41 +0530

    filter component

--- src/Pages/Plays.page.js
+++ src/Pages/Plays.page.js
@@ -1,186 +1,84 @@
 import React from "react";
-
+import Poster from "../components/Poster/Poster.component.js";
+import PlaysFilter from "../components/PlaysFilters/PlaysFilters.component.js";
 const Plays = () => {
-  return (
-    <>
-      <div className="flex justify-end p-16 ml-80 flex-col">
-        <div>
-          <h1 className="text-gray-700 text-2xl font-bold py-4">
-            Plays in Bhubaneshwar
-          </h1>
-        </div>
-
-        <div className="flex flex-wrap w-full h-full gap-8 ">
-
-
-          <div className="flex flex-col py-2 ">
-            <div className="w-full h-full">
-              <img
-                src="https://assets-in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:w-400.0,h-660.0,cm-pad_resize,bg-000000,fo-top:ote-U3VuLCAxOCBKdW4%3D,ots-29,otc-FFFFFF,oy-612,ox-24:q-80/et00357799-vfegxnntfv-portrait.jpg"
-                alt="chakravyuh"
-                className="w-56 h-88 rounded-lg"
-              />
-            </div>
-            <div className="flex flex-col py-2 my-auto">
-              <div className="flex flex-wrap w-56 pt-4">
-                <h1 className="font-bold">
-                  Chakravyuh featuring Nitish Bharadwaj as Krishna
-                </h1>
-              </div>
-              <h4>Kamani Auditorium:Delhi</h4>
-              <h4 className="text-gray-600">Hindi</h4>
-              <h4 className="text-gray-600">₹ 600 onwards</h4>
-            </div>
-          </div>
-
-
-          <div className="flex flex-col py-2 ">
-            <div className="w-full h-full">
-              <img
-                src="https://assets-in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:w-400.0,h-660.0,cm-pad_resize,bg-000000,fo-top:ote-VGh1LCAxMyBKdWwgb253YXJkcw%3D%3D,ots-29,otc-FFFFFF,oy-612,ox-24:q-80/et00360973-jgjblfsffk-portrait.jpg"   
-                alt="Vanika"
-                className="w-56 h-88 rounded-lg"
-              />
-            </div>
-            <div className="flex flex-col py-2 my-auto">
-              <div className="flex flex-wrap w-56 pt-4">
-                <h1 className="font-bold">
-                Vanika Sangtani - Moving on (Storytelling Solo)                </h1>
-              </div>
-              <h4>Kamani Auditorium:Delhi</h4>
-              <h4 className="text-gray-600">Hindi</h4>
-              <h4 className="text-gray-600">₹ 400 onwards</h4>
-            </div>
-          </div>
-
-
-          <div className="flex flex-col py-2 ">
-            <div className="w-full h-full">
-              <img
-                src="https://assets-in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:w-400.0,h-660.0,cm-pad_resize,bg-000000,fo-top:ote-U3VuLCAxOCBKdW4%3D,ots-29,otc-FFFFFF,oy-612,ox-24:q-80/et00357710-yqarwkvglv-portrait.jpg"
-                alt="Open Mic"
-                className="w-56 h-88 rounded-lg"
-              />
-            </div>
-            <div className="flex flex-col py-2 my-auto">
-              <div className="flex flex-wrap w-56 pt-4">
-                <h1 className="font-bold">
-                Delhi Open Mic : Guftagu Cafe                </h1>
-              </div>
-              <h4>Kamani Auditorium:Delhi</h4>
-              <h4 className="text-gray-600">Hindi</h4>
-              <h4 className="text-gray-600">₹ 600 onwards</h4>
-            </div>
-          </div>
-
-
-          <div className="flex flex-col py-2 ">
-            <div className="w-full h-full">
-              <img
-                src="https://assets-in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:w-400.0,h-660.0,cm-pad_resize,bg-000000,fo-top:ote-U3VuLCAxNiBKdWw%3D,ots-29,otc-FFFFFF,oy-612,ox-24:q-80/et00351855-zrwxreebur-portrait.jpg"
-                alt="Hamlet"
-                className="w-56 h-88 rounded-lg"
-              />
-            </div>
-            <div className="flex flex-col py-2 my-auto">
-              <div className="flex flex-wrap w-56 pt-4">
-                <h1 className="font-bold">
-                Hamlet                </h1>
-              </div>
-              <h4>Kamani Auditorium:Delhi</h4>
-              <h4 className="text-gray-600">Hindi</h4>
-              <h4 className="text-gray-600">₹ 200 onwards</h4>
-            </div>
-          </div>
-
-
-          <div className="flex flex-col py-2 ">
-            <div className="w-full h-full">
-              <img
-                src="https://assets-in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:w-400.0,h-660.0,cm-pad_resize,bg-000000,fo-top:ote-U2F0LCAxIEp1bA%3D%3D,ots-29,otc-FFFFFF,oy-612,ox-24:q-80/et00361798-lgjlhbngcs-portrait.jpg"
-                alt="Poets"
-                className="w-56 h-88 rounded-lg"
-              />
-            </div >
-            <div className="flex flex-col py-2 my-auto" >
-              <div className="flex flex-wrap w-56 pt-4">
-                <h1 className="font-bold">
-                  Accidental Poets
-                </h1>
-              </div>
-              <h4>Kamani Auditorium:Delhi</h4>
-              <h4 className="text-gray-600">Hindi</h4>
-              <h4 className="text-gray-600">₹ 500 onwards</h4>
-            </div>
-          </div>
-
-
-          <div className="flex flex-col py-2 ">
-            <div className="w-full h-full">
-              <img
-                src="https://assets-in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:w-400.0,h-660.0,cm-pad_resize,bg-000000,fo-top:ote-RnJpLCAxNiBKdW4%3D,ots-29,otc-FFFFFF,oy-612,ox-24:q-80/et00343787-ywkycduujz-portrait.jpg"
-                alt="Tansen"
-                className="w-56 h-88 rounded-lg"
-              />
-            </div>
-            <div className="flex flex-col py-2 my-auto">
-              <div className="flex flex-wrap w-56 pt-4">
-                <h1 className="font-bold">Tansen</h1>
-              </div>
-              <h4>Kamani Auditorium:Delhi</h4>
-              <h4 className="text-gray-600">Hindi</h4>
-              <h4 className="text-gray-600">₹ 400 onwards</h4>
-            </div>
-          </div>
-
-
-          <div className="flex flex-col py-2 ">
-            <div className="w-full h-full">
-              <img
-                src="https://assets-in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:w-400.0,h-660.0,cm-pad_resize,bg-000000,fo-top:ote-U3VuLCAyNSBKdW4%3D,ots-29,otc-FFFFFF,oy-612,ox-24:q-80/et00332735-dkmydfmmud-portrait.jpg"
-                alt="Akbar"
-                className="w-56 h-88 rounded-lg"
-              />
-            </div>
-            <div className="flex flex-col py-2 my-auto">
-              <div className="flex flex-wrap w-56 pt-4">
-                <h1 className="font-bold">
-                AKBAR THE GREAT NAHIN RAHE
-                </h1>
-              </div>
-              <h4>Kamani Auditorium:Delhi</h4>
-              <h4 className="text-gray-600">Hindi</h4>
-              <h4 className="text-gray-600">₹ 300 onwards</h4>
-            </div>
-          </div>
-
-
-          <div className="flex flex-col py-2">
-            <div className="w-full h-full">
-              <img
-                src="https://assets-in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:w-400.0,h-660.0,cm-pad_resize,bg-000000,fo-top:ote-RnJpLCAzMCBKdW4%3D,ots-29,otc-FFFFFF,oy-612,ox-24:q-80/et00361722-ejzkpuusne-portrait.jpg"
-                alt="Tax Free"
-                className="w-56 h-88 rounded-lg"
-              />
-            </div>
-
-            <div className="flex flex-col py-2 my-auto">
-              <div className="flex flex-wrap w-56 pt-4">
-                <h1 className="font-bold">TAX FREE</h1>
-              </div>
-              <h4>Kamani Auditorium:Delhi</h4>
-              <h4 className="text-gray-600">Hindi</h4>
-              <h4 className="text-gray-600">₹ 200 onwards</h4>
-            </div>
-          </div>
-
-
-
-
-        </div>
+return (
+<>
+<div className="container mx-auto px-4 py-16">
+   <div className="w-full lg:flex lg:flex-row-reverse">
+      <div className="lg:w-8/12">
+      <h2 className="text-2xl font-bold mb-4">Plays in Bhubaneswar</h2>
+      <div className="flex flex-wrap">
+         <div className="w-1/2 md:w-1/3 my-3 lg:w-1/4">
+            <Poster
+               src="https://assets-in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:w-400.0,h-660.0,cm-pad_resize,bg-000000,fo-top:ote-U3VuLCAxOCBKdW4%3D,ots-29,otc-FFFFFF,oy-612,ox-24:q-80/et00357799-vfegxnntfv-portrait.jpg"
+               title="Chakravyuh featuring Nitish Bharadwaj as..."
+               subtitle="Tamil ₹300"
+               />
+         </div>
+         <div className="w-1/2 md:w-1/3 my-3 lg:w-1/4">
+            <Poster
+               src="https://assets-in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:w-400.0,h-660.0,cm-pad_resize,bg-000000,fo-top:ote-U2F0LCAxNyBKdW4gb253YXJkcw%3D%3D,ots-29,otc-FFFFFF,oy-612,ox-24:q-80/et00360216-walsncbknh-portrait.jpg"
+               title="The F word"
+               subtitle="Tamil ₹300"
+               />
+         </div>
+         <div className="w-1/2 md:w-1/3 my-3 lg:w-1/4">
+            <Poster
+               src="https://assets-in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:w-400.0,h-660.0,cm-pad_resize,bg-000000,fo-top:ote-U3VuLCAyIEp1bA%3D%3D,ots-29,otc-FFFFFF,oy-612,ox-24:q-80/et00361238-cgyrctfqkp-portrait.jpg"
+               title="I am from Delhi(Bihar)"
+               subtitle="Tamil ₹300"
+               />
+         </div>
+         <div className="w-1/2 md:w-1/3 my-3 lg:w-1/4">
+            <Poster
+               src="https://assets-in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:w-400.0,h-660.0,cm-pad_resize,bg-000000,fo-top:ote-RnJpLCAxNiBKdW4%3D,ots-29,otc-FFFFFF,oy-612,ox-24:q-80/et00343787-ywkycduujz-portrait.jpg"
+               title="Tansen"
+               subtitle="Tamil ₹300"
+               />
+         </div>
+         <div className="w-1/2 md:w-1/3 my-3 lg:w-1/4">
+            <Poster
+               src="https://assets-in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:w-400.0,h-660.0,cm-pad_resize,bg-000000,fo-top:ote-U3VuLCAxOCBKdW4%3D,ots-29,otc-FFFFFF,oy-612,ox-24:q-80/et00357799-vfegxnntfv-portrait.jpg"
+               title="Chakravyuh featuring Nitish Bharadwaj as..."
+               subtitle="Tamil ₹300"
+               />
+         </div>
+         <div className="w-1/2 md:w-1/3 my-3 lg:w-1/4">
+            <Poster
+               src="https://assets-in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:w-400.0,h-660.0,cm-pad_resize,bg-000000,fo-top:ote-U2F0LCAxNyBKdW4gb253YXJkcw%3D%3D,ots-29,otc-FFFFFF,oy-612,ox-24:q-80/et00360216-walsncbknh-portrait.jpg"
+               title="The F word"
+               subtitle="Tamil ₹300"
+               />
+         </div>
+         <div className="w-1/2 md:w-1/3 my-3 lg:w-1/4">
+            <Poster
+               src="https://assets-in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:w-400.0,h-660.0,cm-pad_resize,bg-000000,fo-top:ote-U3VuLCAyIEp1bA%3D%3D,ots-29,otc-FFFFFF,oy-612,ox-24:q-80/et00361238-cgyrctfqkp-portrait.jpg"
+               title="I am from Delhi(Bihar)"
+               subtitle="Tamil ₹300"
+               />
+         </div>
+         <div className="w-1/2 md:w-1/3 my-3 lg:w-1/4">
+            <Poster
+               src="https://assets-in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:w-400.0,h-660.0,cm-pad_resize,bg-000000,fo-top:ote-RnJpLCAxNiBKdW4%3D,ots-29,otc-FFFFFF,oy-612,ox-24:q-80/et00343787-ywkycduujz-portrait.jpg"
+               title="Tansen"
+               subtitle="Tamil ₹300"
+               />
+         </div>
       </div>
-    </>
-  );
-};
+      </div>
 
+       <div className="lg:w-1/4">
+        <h2 className="text-2xl font-bold mb-4">Filters</h2>
+        <div>
+        <PlaysFilter title="Date" tags={["Today", "Tomorrow", "This Weekend"]}/>
+        <PlaysFilter title="Language" tags={["Tamil", "Kannada"]}/>
+        <PlaysFilter title="Categories" tags={["Theatre"]}/>
+        </div>
+       </div>
+   </div>
+</div>
+</>
+)
+};
 export default Plays;
